69315b5e4d078e3ab822271c58dd70240cca9127
Christian Hettlage <hettlage@saao.ac.za>
2018-08-22 15:02:02 +0200

    custom error handlers

--- dq_poc/dq_poc.py
+++ dq_poc/dq_poc.py
@@ -4,6 +4,16 @@ from dq_poc.menus import primary_menu, primary_menu_item, secondary_menu, second
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(Exception)
+def generic_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/', defaults={'page': ''})
 @app.route('/dq', defaults={'page': ''})
 @app.route('/dq/<path:page>')
