92311fc08e9ee4945f59b272c6143f7be2965c0b
Christian Hettlage <hettlage@saao.ac.za>
2018-08-22 13:14:47 +0200

    create primary menu dynamically

--- dq_poc/dq_poc.py
+++ dq_poc/dq_poc.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from dq_poc.menus import primary_menu_item
+from dq_poc.menus import primary_menu, primary_menu_item
 
 app = Flask(__name__)
 
@@ -7,4 +7,5 @@ app = Flask(__name__)
 @app.route('/dq/<path:page>')
 def hello(page):
     return render_template('index.html',
+                           primary_menu=primary_menu(),
                            primary_menu_item=primary_menu_item(page))
